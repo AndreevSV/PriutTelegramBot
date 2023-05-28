03e71d081cab54d871b98c8ec315617dcfc78002
AndreevSV <andreevsv@gmail.com>
2023-05-28 22:01:09 +0300

    Первый коммит Master

---
+++ src/test/java/omg/group/priuttelegrambot/PriutTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package omg.group.priuttelegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriutTelegramBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
