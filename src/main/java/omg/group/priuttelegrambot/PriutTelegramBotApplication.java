34d0849a64cf1af5138e2f8742a9e47e329e83e5
AndreevSV <andreevsv@gmail.com>
2023-05-28 22:01:09 +0300

    Initial commit feature-elena

---
+++ src/main/java/omg/group/priuttelegrambot/PriutTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package omg.group.priuttelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriutTelegramBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PriutTelegramBotApplication.class, args);
+    }
+
+}
